cedafb43ddecd0b1358c13c962b84880c1b8ca5b
DarkerThann <105225566+DarkerThann@users.noreply.github.com>
2022-08-17 04:11:29 -0300

    Adicionado calculo de duração da sessão

--- UserInput.cs
+++ UserInput.cs
@@ -1,37 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CodingTracker
+﻿namespace CodingTracker
 {
     internal class UserInput
     {
         private readonly CodingSession coding = CodingSession.GetInstance();
         public void UserValues()
         {
-            
+
             Validation input = new Validation();
 
             Console.WriteLine("Por favor Insira Data e Hora no seguinte Formato: dd/MM/yyyy HH:mm");
 
-            Console.WriteLine("Data e Hora de Inicio");
+            Console.Write("Data e Hora de Inicio: ");
             coding.StartTime = input.HourValidation();
 
-            Console.WriteLine("Data e Hora de Finalização");
+            Console.Write("Data e Hora de Finalização: ");
             coding.EndTime = input.HourValidation();
+
+            TimeSpan hour = coding.StartTime.Subtract(coding.EndTime);            
+            coding.Duration = hour.Duration();
+
         }
 
         public void Id()
         {
-            Console.WriteLine("Informe o ID da sessão: ");
+            Console.Write("Informe o ID da sessão: ");
             try
             {
                 coding.Id = Convert.ToInt32(Console.ReadLine());
             }
-            catch (FormatException)
+            catch (Exception)
             {
                 Console.WriteLine($"ID Inválido. Digite Apenas Números");
                 Id();
