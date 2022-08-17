9bc049a904961a572a1468e663143913c6b9c13b
DarkerThann <105225566+DarkerThann@users.noreply.github.com>
2022-08-17 04:10:42 -0300

    Adicionano tratamento de exeções e opção para voltar ao menu principal ou sair da aplicação

--- Menu.cs
+++ Menu.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CodingTracker
+﻿namespace CodingTracker
 {
     internal class Menu
     {
@@ -14,50 +8,113 @@ namespace CodingTracker
         public void MenuOptions()
         {
             string[] menu = new string[]
-                     {"Digite o número correspondente ao que deseja fazer\n",
+                     {"Menu Principal",
+                     "--------------------------",
                       "0 Sair",
                       "1 Ver todas as sessões",
                       "2 Criar uma nova sessão",
                       "3 Atualizar uma sessão",
-                      "4 Deletar uma sessão\n"
+                      "4 Deletar uma sessão",
+                      "--------------------------",
                      };
 
             foreach (var options in menu)
             {
                 Console.WriteLine(options);
             }
+            controller.CheckIfTableExists();
             MenuChoise();
         }
 
         private void MenuChoise()
         {
-            Console.WriteLine("O que você deseja fazer?");
-            string menuInput = Console.ReadLine();
+            Console.Write("Digite o número da opção: ");
+            byte menuInput = 0;
+            bool condition = true;
+
+            while (condition)
+            {
+                try
+                {
+                    menuInput = Convert.ToByte(Console.ReadLine());
+                    condition = false;
+                    Console.Clear();
+                }
+                catch (Exception)
+                {
+                    Console.Write("Opção inválida.\nPor favor Digite novamente:");
+                }
+            }
 
             switch (menuInput)
             {
-                case "0":
+                case 0:
                     Environment.Exit(0);
                     break;
-                case "1":                   
+                case 1:
                     controller.GetAllCodingSession();
+                    BackMenuOrExit();
                     break;
-                case "2":
+                case 2:
                     input.UserValues();
                     controller.CreateCodingSession();
+                    BackMenuOrExit();
                     break;
-                case "3":
+                case 3:
                     input.Id();
                     input.UserValues();
                     controller.UpdateCodingSession();
+                    BackMenuOrExit();
                     break;
-                case "4":
+                case 4:
                     input.Id();
                     controller.DeleteCodingSession();
+                    BackMenuOrExit();
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida. Por favor Digite novamente.");
+                    MenuOptions();
                     break;
             }
-        
         }
-        
+
+        private void BackMenuOrExit()
+        {
+            Console.Write("--------------------------\n" +
+                          "0 Voltar ao Menu Principal\n" +
+                          "1 Sair da Aplicação\n" +
+                          "--------------------------\n" +
+                          "Digite o número da opção:");
+
+            bool condition = true;
+
+            while (condition)
+            {
+                try
+                {
+                    byte choise = Convert.ToByte(Console.ReadLine());
+                    condition = false;
+                    
+                    if (choise == 0)
+                    {
+                        Console.Clear();
+                        MenuOptions();
+                    }
+                    else
+                    {
+                        Environment.Exit(0);
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.Write("Opção inválida.\nPor favor Digite novamente:");
+                }
+            }
+
+     
+
+
+        }
+
     }
 }
